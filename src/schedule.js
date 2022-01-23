656d603e3620f3d14cb5c256277554fd718ca33b
s-potato <51790227+s-potato@users.noreply.github.com>
2022-01-23 10:33:46 +0700

    change schedule's behavior

--- src/schedule.js
+++ src/schedule.js
@@ -1,9 +1,9 @@
 import cron from 'node-cron';
 import md5 from 'md5';
 import { db } from './firestore.js';
-import { doc, getDocs, collection, Timestamp, updateDoc, arrayUnion } from 'firebase/firestore/lite';
+import { doc, getDocs, collection, Timestamp, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore/lite';
 
-cron.schedule('0 0 1 * *', async ()=>{
+cron.schedule('0 0 * * *', async ()=>{
     var docRef = collection(db, 'money_db');
     const docSnap = await getDocs(docRef);
     docSnap.forEach((document) => {
@@ -11,15 +11,17 @@ cron.schedule('0 0 1 * *', async ()=>{
         var docRef = doc(db, "money_db", `${document.id}`);
         
         document.data().plans.forEach(async (plan)=>{
-            if (plan.expiredAt.seconds > Timestamp.now().seconds) {
-                console.log("true")
+            if (plan.expiredAt.seconds < Timestamp.now().seconds) {
                 await updateDoc(docRef, {
                     transactions: arrayUnion({
                         id: md5(plan.amount.toString() + plan.desc),
                         amount: plan.amount,
                         description: plan.description,
                         type: plan.type,
-                        updatedAt: Timestamp.now()
+                        updatedAt: plan.expiredAt
+                    }),
+                    plans: arrayRemove({
+                        ...plan
                     })
                 })
             }
